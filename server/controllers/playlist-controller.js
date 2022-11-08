5e10f357177f275180f054091325db757febd7ae
mapang02 <marcus.pang@stonybrook.edu>
2022-11-07 22:00:20 -0500

    Delete from user.playlists upon playlist deletion

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -63,9 +63,12 @@ deletePlaylist = async (req, res) => {
                 console.log("req.userId: " + req.userId);
                 if (user._id == req.userId) {
                     console.log("correct user!");
+                    user.playlists.splice(user.playlists.indexOf(req.params.id), 1);
+                    user.save().catch(err => console.log(err));
                     Playlist.findOneAndDelete({ _id: req.params.id }, () => {
                         return res.status(200).json({});
                     }).catch(err => console.log(err))
+
                 }
                 else {
                     console.log("incorrect user!");
